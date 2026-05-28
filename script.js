0ec459cf2a76b8fd29fbd905de41c0a40a2db91a
mihirunshan <156603043+mihirunshan@users.noreply.github.com>
2026-05-28 19:00:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("light-mode");
+
+  // Save theme
+  if (document.body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+// Load saved theme
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "light") {
+    document.body.classList.add("light-mode");
+    toggle.checked = true;
+  }
+});
